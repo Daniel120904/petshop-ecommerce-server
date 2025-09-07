a7839191825d38875b7a65f582328baa342b4f5a
Daniel120904 <danieladriano1209@gmail.com>
2025-09-06 22:05:48 -0300

    ( ´･･)ﾉ(._.`)ಥ_ಥ

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express, { Application } from "express";
+import morgan from "morgan";
+import { PrismaClient } from "../generated/prisma"; 
+
+const app: Application = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err);
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+
+  try {
+    await prisma.$connect();
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+});
