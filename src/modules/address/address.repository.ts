39791e1fbf8a7f6ab985f12f391ce2762d0dcff2
Mahmoud Ghazal <mahmoud.f.ghazal@gmail.com>
2026-06-21 22:29:07 -0300

    fix: deleteAddress

--- src/modules/address/address.repository.ts
+++ src/modules/address/address.repository.ts
@@ -2,6 +2,7 @@ import { TypedRepository } from '../../base/base.typed-repository';
 import { prisma } from '../../core/database/prisma.client';
 
 class AddressRepository extends TypedRepository<typeof prisma.address> {
+    protected hasDeleteFlag = true;
     protected model = prisma.address;
 }
 
