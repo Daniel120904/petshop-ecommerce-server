74d948c31a62af7b45e9dcc7c584ffb29bcca876
Mahmoud Ghazal <mahmoud.f.ghazal@gmail.com>
2026-06-22 21:41:26 -0300

    fix: remove_delete_flag_phone

--- src/modules/phone/phone.repository.ts
+++ src/modules/phone/phone.repository.ts
@@ -2,7 +2,6 @@ import { TypedRepository } from '../../base/base.typed-repository';
 import { prisma } from '../../core/database/prisma.client';
 
 class PhoneRepository extends TypedRepository<typeof prisma.phone> {
-    protected hasDeleteFlag = true;
     protected model = prisma.phone;
 }
 
